8901f49110130d5ff6b81386edf9a97976044e4d
Beatriz <beatrizf9135@gmail.com>
2024-07-25 18:30:06 -0300

    implementacao classe DistribuicaoUniforme

--- Main.java
+++ Main.java
@@ -10,8 +10,14 @@ public class Main {
         estatisticasNormal.imprimirValoresGerados(100);
         estatisticasNormal.imprimirMedia();
         estatisticasNormal.imprimirVariancia();
-    
 
+        DistribuicaoUniforme distribuicaoUniforme = new DistribuicaoUniforme(0, 1);
+        Estatisticas<DistribuicaoUniforme> estatisticasUniforme = new Estatisticas<>(distribuicaoUniforme);
+
+        System.out.println("\nDistribuição Uniforme:");
+        estatisticasUniforme.imprimirValoresGerados(100);
+        estatisticasUniforme.imprimirMedia();
+        estatisticasUniforme.imprimirVariancia();
     }
 }
 
