8b86407ddcdcfe129192c123d5e5f04283b292cf
Luiza Martins <122387125+EstherMart@users.noreply.github.com>
2024-07-28 16:18:25 -0300

    Delete Distribuicao.java

--- Distribuicao.java
+++
@@ -1,8 +0,0 @@
-import java.util.List;
-
-public abstract class Distribuicao {
-    public abstract double gerarMedia();
-    public abstract double gerarVariancia();
-    public abstract double gerarDesvioPadrao();
-    public abstract List<Double> gerarValores(int n);
-}
