01e673fe2a6bbf2afbdbbae742b1651a46d7c4c7
Luiza Martins <122387125+EstherMart@users.noreply.github.com>
2024-07-28 17:18:50 -0300

    Create DistribuicaoPoisson.java

---
+++ src/main/java/DistribuicaoPoisson.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DistribuicaoPoisson extends Distribuicao {
+    
+    private double lambda;
+    private Random random = new Random();
+
+    // Construtor com validação para lambda
+    public DistribuicaoPoisson(double lambda) {
+        if (lambda <= 0) {
+            throw new IllegalArgumentException("O valor de lambda deve ser maior que 0.");
+        }
+        this.lambda = lambda;
+    }
+
+    public double getLambda() {
+        return lambda;
+    }
+
+    @Override
+    public double gerarMedia() {
+        return lambda;
+    }
+
+    @Override
+    public double gerarVariancia() {
+        return lambda;
+    }
+
+    @Override
+    public double gerarDesvioPadrao() {
+        return Math.sqrt(lambda);
+    }
+
+    @Override
+    public List<Double> gerarValores(int n) {
+        List<Double> valores = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            valores.add((double) gerarPoisson(lambda));
+        }
+        return valores;
+    }
+
+    private int gerarPoisson(double lambda) {
+        double L = Math.exp(-lambda);
+        int k = 0;
+        double p = 1.0;
+        do {
+            k++;
+            p *= random.nextDouble();
+        } while (p > L);
+        return k - 1;
+    }
+    
+}
