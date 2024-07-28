fa2f670ac041a9d7da5ef49b2d98cc34b12d0517
Malu404 <136644962+Malu404@users.noreply.github.com>
2024-07-28 16:18:16 -0300

    Create DistribuicaoExponencial.java

---
+++ src/main/java/DistribuicaoExponencial.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DistribuicaoExponencial extends Distribuicao {
+    private double lambda;
+    private Random random;
+
+    public DistribuicaoExponencial(double lambda) {
+        if (lambda <= 0) {
+            throw new IllegalArgumentException("Lambda tem que ser maior que 0");
+        }
+        this.lambda = lambda;
+        this.random = new Random();
+    }
+
+    @Override
+    public double gerarDesvioPadrao() {
+        return Math.sqrt(gerarVariancia());
+    }
+
+    @Override
+    public double gerarVariancia() {
+        return 1 / (lambda * lambda);
+        
+    }
+
+    @Override
+    public double gerarMedia() {
+        return 1 / lambda;
+    }
+
+    @Override
+    public List<Double> gerarValores(int n) {
+        List<Double> valor = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            double u = random.nextDouble();
+            double valores = -Math.log(1 - u) / lambda;
+            valor.add(valores);
+        }
+        return valor;
+    }
+}
