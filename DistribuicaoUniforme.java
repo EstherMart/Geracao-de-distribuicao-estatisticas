0f02e7eeeb216c50394119612b97f3e4aabca8e0
Malu404 <136644962+Malu404@users.noreply.github.com>
2024-07-30 06:55:38 -0300

    Create DistribuicaoUniforme.java

---
+++ DistribuicaoUniforme.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class DistribuicaoUniforme extends Distribuicao {
+    private double minimo;
+    private double maximo;
+    private List<Double> valores;
+
+    Random random = new Random();
+
+    public DistribuicaoUniforme(double minimo, double maximo) {
+        if (minimo >= maximo) {
+            throw new IllegalArgumentException("O valor mínimo tem que ser menor que o valor máximo");
+        }
+        this.minimo = minimo;
+        this.maximo = maximo;
+    }
+
+    @Override
+    public List<Double> gerarValores(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("O número de valores deve ser maior que -1");
+        }
+        valores = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            valores.add(gerarValor());
+        }
+        return valores;
+    }
+
+    @Override
+    public double gerarMedia() {
+        return (minimo + maximo) / 2;
+    }
+
+    @Override
+    public double gerarVariancia() {
+        return Math.pow(maximo - minimo, 2) / 12;
+    }
+
+    @Override
+    public double gerarDesvioPadrao() {
+        return Math.sqrt(gerarVariancia());
+    }
+
+    public double gerarValor() {
+        return minimo + (maximo - minimo) * random.nextDouble();
+    }
+}
